ca4a9eedbbac07f533988fabe847f65f210700c8
Helen Luo <melonhelon1234@gmail.com>
2025-07-14 12:12:47 +0800

    usres.js update

---
+++ actions/users.js
@@ -0,0 +1,31 @@
+"use server";
+
+import { db } from "@/lib/prisma";
+import { clerkClient } from "@clerk/clerk-sdk-node";
+import { auth } from "@clerk/nextjs/server";
+
+export async function updateUsername(username) {
+    const { userId } = auth();
+    if(!userId) {
+        throw new Error("未经授权");
+    }
+
+    const existingUsername = await db.user.findUnique({
+        where:{ username },
+    });
+
+    if(existingUsername && existingUsername.id!==userId) {
+        throw new Error("用户名已被使用");
+    }
+
+    await db.user.update({
+        where:{clerkUserId: userId},
+        data:{username},
+    });
+
+    await clerkClient.users.updateUser(userId, {
+        username,
+    });
+
+    return {success:true};
+}
